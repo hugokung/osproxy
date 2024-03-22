b8d2a2d148e6dfd7b61b33bc874aff84fbf21e72
hugokung <hugokungzdh212@163.com>
2024-03-22 10:22:36 +0800

    fix hard code

--- deploy/build_docker_push.sh
+++ deploy/build_docker_push.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-passwd=$1
-version=$2
-echo {passwd} | docker login --username=qinguoyi --password-stdin
+uname=$1
+passwd=$2
+version=$3
+echo {passwd} | docker login --username=${uname} --password-stdin
 docker build -t object-storage-proxy:${version} -f Dockerfile  .
-docker tag object-storage-proxy:${version} qinguoyi/object-storage-proxy:${version}
-docker push qinguoyi/object-storage-proxy:${version}
+docker tag object-storage-proxy:${version} ${uname}/object-storage-proxy:${version}
+docker push ${uname}/object-storage-proxy:${version}
 if [ $? -eq 0 ]; then
  echo "push success"
 else
